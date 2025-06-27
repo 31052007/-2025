5f954527abcf347723ed0dd9d1d757930035c1ec
Fox Sin <okhlopovskiynikolay@mail.ru>
2025-06-27 11:08:13 +0300

    Add files via upload

---
+++ ConsoleApp1/BluePrinter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class BluePrinter : Printer
+    {
+        public override void Print(string value) 
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(value);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Массив базового типа Printer
+            Printer[] printers = new Printer[]
+            {
+                new Printer(),
+                new RedPrinter(),
+                new GreenPrinter(),
+                new BluePrinter()
+            };
+
+           for (int i = 0;i< 4; i++)
+            {
+               
+                Console.ResetColor();
+
+                Console.WriteLine("Доброго времени суток");
+            }
+            
+            
+            //// Перебираем и вызываем метод Print 
+            //foreach (var printer in printers)
+            //{
+            //    // приведение к типу Printer 
+            //    Printer p = (Printer)printer;
+            //    p.Print("Доброго времени суток!");
+            //}
+
+           
+            Console.ReadKey();
+        }
+    }
+}
+
